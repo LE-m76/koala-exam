11ef3b9f090a28333801728e31fd1d4d2c8f9ac2
21482 <2148270572@qq.com>
2026-05-11 15:23:02 +0800

    first commit: add exam files

---
+++ js/index.js
@@ -0,0 +1,187 @@
+const questionList = [{
+		id: 1,
+		title: '下列哪个是java的基础数据类型',
+		type: 1,
+		options: [{
+				id: 1,
+				name: 'string',
+				label: 'A'
+			},
+			{
+				id: 2,
+				name: 'int',
+				label: 'B'
+			},
+			{
+				id: 3,
+				name: 'List',
+				label: 'C'
+			},
+			{
+				id: 4,
+				name: 'Map',
+				label: 'D'
+			}
+		],
+		answer: ['2'],
+		answered: false,
+		correct: false,
+		fillAnswer: '', // 填空用
+		essayAnswer: '' // 简答用
+	},
+	{
+		id: 2,
+		title: '在Java中，以下哪个关键字用于定义常量？',
+		type: 1,
+		options: [{
+				id: 1,
+				name: 'final',
+				label: 'A'
+			},
+			{
+				id: 2,
+				name: 'static',
+				label: 'B'
+			},
+			{
+				id: 3,
+				name: 'const',
+				label: 'C'
+			},
+			{
+				id: 4,
+				name: 'var',
+				label: 'D'
+			}
+		],
+		answer: ['1'],
+		answered: false,
+		correct: false,
+		fillAnswer: '', // 填空用
+		essayAnswer: '' // 简答用
+	},
+	{
+		id: 3,
+		title: 'Java中，main方法的返回值类型是？',
+		type: 1,
+		options: [{
+				id: 1,
+				name: 'int',
+				label: 'A'
+			},
+			{
+				id: 2,
+				name: 'void',
+				label: 'B'
+			},
+			{
+				id: 3,
+				name: 'String',
+				label: 'C'
+			},
+			{
+				id: 4,
+				name: 'boolean',
+				label: 'D'
+			}
+		],
+		answer: ['2'],
+		answered: false,
+		correct: false,
+		fillAnswer: '', // 填空用
+		essayAnswer: '' // 简答用
+	},
+	{
+		id: 4,
+		title: '以下哪个是Java的访问修饰符？',
+		type: 1,
+		options: [{
+				id: 1,
+				name: 'for',
+				label: 'A'
+			},
+			{
+				id: 2,
+				name: 'if',
+				label: 'B'
+			},
+			{
+				id: 3,
+				name: 'private',
+				label: 'C'
+			},
+			{
+				id: 4,
+				name: 'while',
+				label: 'D'
+			}
+		],
+		answer: ['3'],
+		answered: false,
+		correct: false,
+		fillAnswer: '', // 填空用
+		essayAnswer: '' // 简答用
+	},
+	{
+		id: 5,
+		title: '在Java中，用来创建对象的关键字是？',
+		type: 1,
+		options: [{
+				id: 1,
+				name: 'class',
+				label: 'A'
+			},
+			{
+				id: 2,
+				name: 'new',
+				label: 'B'
+			},
+			{
+				id: 3,
+				name: 'this',
+				label: 'C'
+			},
+			{
+				id: 4,
+				name: 'static',
+				label: 'D'
+			}
+		],
+		answer: ['2'],
+		answered: false,
+		correct: false,
+		fillAnswer: '', // 填空用
+		essayAnswer: '' // 简答用
+	},
+	{
+		id: 6,
+		title: 'Java中，以下哪个循环结构适合遍历数组？',
+		type: 1,
+		options: [{
+				id: 1,
+				name: 'switch',
+				label: 'A'
+			},
+			{
+				id: 2,
+				name: 'for-each',
+				label: 'B'
+			},
+			{
+				id: 3,
+				name: 'if-else',
+				label: 'C'
+			},
+			{
+				id: 4,
+				name: 'try-catch',
+				label: 'D'
+			}
+		],
+		answer: ['2'],
+		answered: false,
+		correct: false,
+		fillAnswer: '', // 填空用
+		essayAnswer: '' // 简答用
+	}
+]
